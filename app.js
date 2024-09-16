1b4a5397d7540f1674b4ba9633e09fbc0ae6493f
sauravztx <saurabh.ztx@gmail.com>
2024-09-16 09:43:28 +0530

    Seperatin of concerns and Testing using postman

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const db = require('./config/mongooseConnection');
+const ownersRouter = require('./routes/ownersRouter')
+const usersRouter = require('./routes/usersRouter')
+const productsRouter = require('./routes/productsRouter')
+
+app.use(express.json());
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieParser());
+
+app.use('/owners', ownersRouter);
+app.use('/users', usersRouter);
+app.use('/products', productsRouter);
+
+app.listen(3000);
