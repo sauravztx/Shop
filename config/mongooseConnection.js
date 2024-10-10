ab91bfa6624b295583e9b8807574c821c7bcf95f
sauravztx <saurabh.ztx@gmail.com>
2024-10-10 16:24:05 +0530

    Seperation of concerns

--- config/mongooseConnection.js
+++ config/mongooseConnection.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
+const config =  require('config');
+const dbgr = require('debug')("development:mongoose");
 
-mongoose.connect('mongodb://127.0.0.1:27017/shop')
+mongoose.connect(`${config.get("MONGODB_URL")}/shop`)
 .then(function(){
-    console.log("connected");
+   dbgr("connected");
 })
 .catch(function(err){
-    console.log(err);
+    dbgr(err);
 })
 
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,6 +1,23 @@
 const express = require('express');
-
 const router = express.Router();
+const ownerModel = require('../models/ownerModel');
+
+if(process.env.NODE_ENV === "development"){
+    router.post('/create', async function(req, res) {
+        let owners = await ownerModel.find();
+        if(owners.length > 0){
+            return res.status(503).send("you dont't have permission to create a new owner.");
+        }
+        let {fullName, email, password} = req.body;
+
+        let createdOwner = await ownerModel.create({
+            fullName,
+            email,
+            password
+        })
+        res.status(201).send(createdOwner);
+    });
+}
 
 router.get('/', function(req, res){
     res.send("hey its owners page");
